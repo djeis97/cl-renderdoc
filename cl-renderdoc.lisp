aa43483f5f397d6c66ce8f237caff774cc2188a2
Elijah Malaby <qwe12345678910@gmail.com>
2019-04-20 22:05:42 -0400

    Added/exported basic control functions

--- cl-renderdoc.lisp
+++ cl-renderdoc.lisp
@@ -9,14 +9,18 @@
 
 (cffi:defcfun ("RENDERDOC_GetAPI" renderdoc-get-api%) :int (v :int) (p :pointer))
 
-(cl:defun renderdoc-get-api (version)
+(cl:defun renderdoc-get-api ()
   (with-alloc (p :pointer)
-    (renderdoc-get-api% version p)
+    (renderdoc-get-api% +e-renderdoc-api-version-1-3-0+ p)
     (make-renderdoc-api-1-3-0 :ptr (cffi:mem-ref p :pointer))))
 
-(cl:defun renderdoc-get-api-version (renderdoc-api)
+(cl:defparameter *renderdoc-api-handle* (renderdoc-get-api))
+
+(cl:in-package :cl-renderdoc)
+
+(defun get-api-version (&optional (renderdoc-api-handle *renderdoc-api-handle*))
   (with-alloc (i :int 3)
-    (cffi:foreign-funcall-pointer (renderdoc-api-1-3-0.get-api-version renderdoc-api) ()
+    (cffi:foreign-funcall-pointer (renderdoc-api-1-3-0.get-api-version renderdoc-api-handle) ()
 				  :pointer (c-aptr i 0 :int)
 				  :pointer (c-aptr i 1 :int)
 				  :pointer (c-aptr i 2 :int)
@@ -24,3 +28,13 @@
     (cl:values (c-aref i 0 :int)
 	       (c-aref i 1 :int)
 	       (c-aref i 2 :int))))
+
+(defun launch-replay-ui (&optional (renderdoc-api-handle *renderdoc-api-handle*))
+  (cffi:foreign-funcall-pointer (renderdoc-api-1-3-0.launch-replay-ui renderdoc-api-handle) ()
+				:uint 1 :pointer (cffi:null-pointer)
+				:uint))
+
+(defun trigger-capture (&optional (renderdoc-api-handle *renderdoc-api-handle*))
+  (cffi:foreign-funcall-pointer (renderdoc-api-1-3-0.trigger-capture renderdoc-api-handle) () :void))
+
+
--- package.lisp
+++ package.lisp
@@ -1,7 +1,10 @@
 ;;;; package.lisp
+(cl:in-package #:cl)
 
 (defpackage #:cl-renderdoc-int
+  (:export #:*renderdoc-api-handle*)
   (:use #:autowrap))
 
 (defpackage #:cl-renderdoc
-  (:use #:cl))
+  (:use #:cl #:autowrap #:cl-renderdoc-int)
+  (:export #:get-api-version #:launch-replay-ui #:trigger-capture))
